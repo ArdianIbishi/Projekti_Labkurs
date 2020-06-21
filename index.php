30b383edd546266d16105209b2f62b4216653f21
Ardian Ibishi <ardian.ibishi2016@gmail.com>
2020-06-22 00:24:35 +0200

    project start

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Projekti</title>
+        <meta charset="UTF-8">
+        <link rel="Stylesheet" href="projekti.css">
+    </head>
+
+    <body>
+        <div id="container">
+            <div class="header1">
+                <div class="menu-majtas">menu majtas</div>
+                <div class="menu-djathtas">menu djathtas</div>
+            </div>
+            <div class="header2">
+                <div id="logo">logo</div>
+                <div id="search">search</div>
+                <div id="supermarket">SUPERMARKET</div>
+            </div>
+            <div class="middlecontainer">
+                <div id="kategorite">Kategorite</div>
+                <div id="slidet">Slidet</div>
+            </div>
+            <div class="artikujt">
+                <div class="artikulli" id="first">artikulli1</div>
+                <div class="artikulli" id="second">artikulli2</div>
+                <div class="artikulli" id="thirst">artikulli3</div>
+                <div class="artikulli" id="fourth">artikulli4</div>
+                <div class="artikulli" id="five">artikulli5</div>
+                
+            </div>
+            <div class="footer">
+                <div class="sherbimi" id="qmimi">qmimi</div>
+                <div class="sherbimi" id="transporti">transporti</div>
+                <div class="sherbimi" id="kthimi">kthimi</div>
+                
+            </div>
+        </div>
+    </body>
+</html>
