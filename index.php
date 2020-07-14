2cf4190442b9fdb82c5b72d6266875f3189ffcf8
Ardian Ibishi <ardian.ibishi2016@gmail.com>
2020-07-15 01:05:26 +0200

    add some changes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <title>Projekti</title>
         <meta charset="UTF-8">
-        <link rel="Stylesheet" href="projekti.css">
+        <link rel="Stylesheet" href="projekti1.css">
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
         
             
@@ -76,26 +76,133 @@
                 <div id="supermarket"><h2>SUPERMARKET</h2></div>
             </div>
             <div class="middlecontainer">
-                <div id="kategorite"><h3 class="h3">KATEGORITE<i class="fa fa-chevron-down"></i><h3>
+
+                 <div id="kategorite"><h3 class="h3">KATEGORITE<i class="fa fa-chevron-down"></i><h3>
                     <ul><li class="li-first">
-                        <a href="#"><i class="fa fa-laptop"></i>Kompjuter&Kancelari</a></li>
-                        <li class="li-second"><a href="#"><i class="fa fa-home" aria-hidden="true"></i>
-                            Shtepia</a></li>
-                            <li class="li-first"><a href="#"><i class="fa fa-plug" aria-hidden="true"></i>
-                                Elektronike</a></li>
-                                <li class="li-second"><a href="#"><i class="fa fa-tablet" aria-hidden="true"></i>
-                                    Smartphone&Tablet</a></li>
-                                    <li class="li-first"><a href="#"><i class="fa fa-female"></i>Femra</a></li>
-                                    <li class="li-second"><a href="#"><i class="fa fa-male" aria-hidden="true"></i>
-                                        Meshkuj</a></li>
-                                        <li class="li-first"><a href="#"><i class="fa fa-gamepad" aria-hidden="true"></i>
-                                            Femije&Lodra</a></li>
-                                            <li class="li-second"><a href="#"><i class="fa fa-leaf" aria-hidden="true"></i>
-                                                Kujdesi Personal</a></li>
-                                                <li class="li-first"><a href="#"><i class="fa fa-car" aria-hidden="true"></i>
-                                                    Makina Aksesore</a></li>
-                                                    <li class="li-second"><a href="#"><i class="fa fa-book" aria-hidden="true"></i>
-                                                        Libra</a></li>
+
+                        <div class="drop1">
+                            <h4 class="drop1"><b><a href="#"><i class="fa fa-laptop"></i>Kompjuter&Kancelari</a><b></h4>
+                            <div class="drop3">
+                              <a href="#"><b>Na kontaktoni<b></a>
+                              <a href="#"><b>Transporti<b></a>
+                              <a href="#"><b>Kthimi i produktit<b></a>
+                            </div>
+
+
+                        </li>
+                        <li class="li-second">
+                            <div class="drop1">
+                                <h4 class="drop1"><b>   <a href="#"><i class="fa fa-home" aria-hidden="true"></i>
+                                    Shtepia </a><b></h4>
+                                <div class="drop3">
+                                  <a href="#"><b>Na kontaktoni<b></a>
+                                  <a href="#"><b>Transporti<b></a>
+                                  <a href="#"><b>Kthimi i produktit<b></a>
+                                </div>
+                            
+                         </li>
+                            <li class="li-first">
+                                
+                                <div class="drop1">
+                                    <h4><b><a href="#"><i class="fa fa-plug" aria-hidden="true"></i>
+                                        Elektronike</a><b></h4>
+                                    <div class="drop3">
+                                      <a href="#"><b>Na kontaktoni<b></a>
+                                      <a href="#"><b>Transporti<b></a>
+                                      <a href="#"><b>Kthimi i produktit<b></a>
+                                    </div>
+                                
+                                </li>
+                                <li class="li-second">
+                                    
+                                    
+                                <div class="drop1">
+                                    <h4><b><a href="#"><i class="fa fa-tablet" aria-hidden="true"></i>
+                                        Smartphone&Tablet</a><b></h4>
+                                    <div class="drop3">
+                                      <a href="#"><b>Na kontaktoni<b></a>
+                                      <a href="#"><b>Transporti<b></a>
+                                      <a href="#"><b>Kthimi i produktit<b></a>
+                                    </div>
+                                
+                                    
+                                    
+                                    </li>
+                                    <li class="li-first">
+                                        
+                                        
+                                <div class="drop1">
+                                    <h4>
+                                      <b>  <a href="#"><i class="fa fa-female"></i>Femra</a><b></h4>
+                                    <div class="drop3">
+                                      <a href="#"><b>Na kontaktoni<b></a>
+                                      <a href="#"><b>Transporti<b></a>
+                                      <a href="#"><b>Kthimi i produktit<b></a>
+                                    </div>
+                                
+                                    </li>
+                                    <li class="li-second">
+                                        
+                                        <div class="drop1">
+                                            <h4 class="drop1"><b><a href="#"><i class="fa fa-male" aria-hidden="true"></i>
+                                                Meshkuj</a><b></h4>
+                                            <div class="drop3">
+                                              <a href="#"><b>Na kontaktoni<b></a>
+                                              <a href="#"><b>Transporti<b></a>
+                                              <a href="#"><b>Kthimi i produktit<b></a>
+                                            </div>
+                                        </li>
+                                        <li class="li-first">
+                                            
+                                            <div class="drop1">
+                                                <h4 class="drop1"><b><a href="#"><i class="fa fa-gamepad" aria-hidden="true"></i>
+                                                    Femije&Lodra</a><b></h4>
+                                                <div class="drop3">
+                                                  <a href="#"><b>Na kontaktoni<b></a>
+                                                  <a href="#"><b>Transporti<b></a>
+                                                  <a href="#"><b>Kthimi i produktit<b></a>
+                                                </div>
+                                            
+                                            </li>
+                                            <li class="li-second">
+                                                
+                                                <div class="drop1">
+                                                    <h4 class="drop1"><b><a href="#"><i class="fa fa-leaf" aria-hidden="true"></i>
+                                                        Kujdesi Personal</a><b></h4>
+                                                    <div class="drop3">
+                                                      <a href="#"><b>Na kontaktoni<b></a>
+                                                      <a href="#"><b>Transporti<b></a>
+                                                      <a href="#"><b>Kthimi i produktit<b></a>
+                                                    </div>
+                                                
+                                                </li>
+                                                <li class="li-first">
+                                                    
+                                                    <div class="drop1">
+                                                        <h4 class="drop1"><b><a href="#"><i class="fa fa-car" aria-hidden="true"></i>
+                                                            Makina Aksesore</a></i><b></h4>
+                                                        <div class="drop3">
+                                                          <a href="#"><b>Na kontaktoni<b></a>
+                                                          <a href="#"><b>Transporti<b></a>
+                                                          <a href="#"><b>Kthimi i produktit<b></a>
+                                                        </div>
+                                                    
+                                                    
+                                                    </li>
+                                                    <li class="li-second">
+                                                        
+                                                        
+                                                    <div class="drop1">
+                                                        <h4 class="drop1"><b><a href="#"><i class="fa fa-book" aria-hidden="true"></i>
+                                                            Libra</a></i><b></h4>
+                                                        <div class="drop3">
+                                                          <a href="#"><b>Na kontaktoni<b></a>
+                                                          <a href="#"><b>Transporti<b></a>
+                                                          <a href="#"><b>Kthimi i produktit<b></a>
+                                                        </div>
+                                                    
+                                                        
+                                                        </li>
 
 
                     </ul>
